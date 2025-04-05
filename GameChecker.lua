fe0e073b98e88f71830e567168383cb4a8085ffa
Snow Lover <louis.martin312@icloud.com>
2025-04-05 13:17:42 +0200

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,7 +1,7 @@
 local BuildABoat = 537413528
 
 if game.PlaceId = BuildABoat do
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua")
 else
-  warn("Game not supported")
+    warn("Game not supported")
 end
