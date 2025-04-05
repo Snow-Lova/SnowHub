f9adf0070bd3f7ae6bd16b10dfcfc6fb6dcd273b
Snow Lover <louis.martin312@icloud.com>
2025-04-05 13:24:54 +0200

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,6 +1,5 @@
 if game.PlaceId == 537413528 then
-    task.wait(1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua"))()
 else
     warn("Game not supported")
 end
