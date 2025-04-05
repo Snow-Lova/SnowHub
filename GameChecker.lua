aaf46a25869ad55362caa037b353a8ac10ece48e
Snow Lover <louis.martin312@icloud.com>
2025-04-05 13:23:48 +0200

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,6 +1,4 @@
-local BuildABoat = 537413528
-
-if game.PlaceId == BuildABoat then
+if game.PlaceId == 537413528 then
     task.wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua")
 else
