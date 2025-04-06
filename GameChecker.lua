0126031984e7c376d95e397fa08e0c1fadac10e8
Snow Lover <louis.martin312@icloud.com>
2025-04-06 07:32:58 +0200

    GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,5 +1,9 @@
-if game.PlaceId == 537413528 then
+local BuildABoat = 537413528
+local BlendingSim2 = 6170705260
+if game.PlaceId == BuildABoat then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua"))()
+elseif game.PlaceId == BlendingSim2 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/2.lua"))()
 else
     warn("Game not supported")
 end
