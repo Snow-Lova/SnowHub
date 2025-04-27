30268f906e5924e763a2bcff17e9f940e601e7fc
Snow Lover <louis.martin312@icloud.com>
2025-04-27 17:19:56 +0200

    Improved game checker

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,10 +1,16 @@
-local BuildABoat = 537413528
-local BlendingSim2 = 6170705260
-if game.PlaceId == BuildABoat then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/1.lua"))()
-elseif game.PlaceId == BlendingSim2 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/2.lua"))()
+local link = "https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/"
+
+local games = {
+    [537413528] = "1.lua", -- Build A Boat
+    [6170705260] = "2.lua", -- Blending Simulator 2
+    [87700573492940] = "4.lua", -- Untitled Drill Game
+}
+
+local file = games[game.PlaceId]
+
+if file then
+    loadstring(game:HttpGet(link .. file))()
 else
     warn("Game not supported")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Snow-Lova/SnowHub/refs/heads/main/Unsupported.lua"))()
+    loadstring(game:HttpGet(link .. "Unsupported.lua"))()
 end
